18ffe51ac9d8c2846c3e9718d0595f555e24ee1c
Yoni Friedman <yoniifriedman@gmail.com>
2024-06-03 14:34:14 -0400

    added truncated normal

--- distribution_utils.jl
+++ distribution_utils.jl
@@ -1,22 +1,10 @@
 using Gen
+using Distributions
 
 function uniprobs(vec)
 	probs = fill(1/length(vec), length(vec))
 	return probs
 end
-
-# TODO: fix this
-@dist function truncated_normal(mu, sigma, lower, upper)
-	normal(mu, sigma)
-end
-
-# @dist binned_normal_2d(mu_x, mu_y, sigma_x, sigma_y, bins)
-# 	x = normal(mu_x, sigma_x)
-# 	y = normal(mu_y, sigma_y)
-# 	bin_x = Int(trunc(x))
-# 	bin_y = Int(trunc(y))
-# 	(bin_x, bin_y)
-# end
 
 @dist function uniform_cat(size::Int)
 	probs = fill(1/size, size)
@@ -27,3 +15,21 @@ end
 	index = categorical(probs)
 	labels[index]
 end
+
+struct TruncNorm <: Gen.Distribution{Float64} end
+
+const trunc_norm = TruncNorm()
+
+function Gen.logpdf(::TruncNorm, value::Float64, mu::Float64, sigma::Float64, low::Float64, high::Float64)
+	d = Truncated(Normal(mu, sigma), low, high)
+	return Distributions.logpdf(d, value)
+end
+
+function Gen.random(::TruncNorm, mu::Float64, sigma::Float64, low::Float64, high::Float64)
+	d = Truncated(Normal(mu, sigma), low, high)
+	return rand(d)
+end
+
+logpdf_grad(::TruncNorm) = nothing 
+has_output_grad(::TruncNorm) = false
+has_argument_grads(::TruncNorm) = (false, false, false, false)
