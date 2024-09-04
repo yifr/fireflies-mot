688d302a2887a57e4a3f770d35a2c5739979cce4
Yoni Friedman <yoniifriedman@gmail.com>
2024-09-04 12:58:40 -0400

    forgot to add config file

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import jax.numpy as jnp
 
-SCENE_SIZE = jnp.int16(64)
-MIN_VELOCITY = -2.
-MAX_VELOCITY = 2.
+SCENE_SIZE = jnp.int16(32)
+MIN_VELOCITY = jnp.float32(-2.)
+MAX_VELOCITY = jnp.float32(2.)
 SCENE_Y, SCENE_X = jnp.mgrid[0:SCENE_SIZE, 0:SCENE_SIZE] 
-TIME_STEPS = 20
-MAX_GLOW_SIZE = 1.5
+TIME_STEPS = 10
+MAX_GLOW_SIZE = jnp.float32(1.)
