c8827672160bf505b3bec83c5c882b42a0a86102
Yoni Friedman <yoniifriedman@gmail.com>
2024-09-10 13:59:33 -0400

    slight improvement in rendering code, clean test file for simulating

--- render.py
+++ render.py
@@ -5,27 +5,21 @@ from genjax import gen
 from config import *
 
 @jax.jit
-def calculate_glow(coordinate, glow_size):
-    distances = jnp.sqrt((SCENE_X - jnp.round(coordinate[1]))**2 + (SCENE_Y - jnp.round(coordinate[0]))**2)
-    glow_values = jnp.exp(-(distances**2) / (2 * glow_size**2))
-    return glow_values
+def gaussian_2d(loc, scale):
+    """
+    Compute a 2D gaussian on a grid, with a center and size specified by loc, scale
+    """
+    x, y = loc
+    distances = jnp.sqrt((SCENE_X - jnp.round(x))**2 + (SCENE_Y - jnp.round(y))**2)
+    values = jnp.exp(-(distances**2) / (2 * scale ** 2))
+    return values
 
 @jax.jit
 def render_frame(xs, ys, blinks, state_durations):
-    # Extract x, y coordinates and blinking values for the current frame
-    coordinates = jnp.stack([xs, ys], axis=-1)
-    blink_durations = state_durations * blinks
-    glow_sizes = jnp.where(blink_durations < MAX_GLOW_SIZE, blink_durations, MAX_GLOW_SIZE)
-    glow_values = jax.vmap(calculate_glow, in_axes=(0, 0))(coordinates, glow_sizes)
-
-    # TODO: speed this up getting rid of scan
-    glow_grid = jnp.zeros((SCENE_SIZE, SCENE_SIZE)) 
-
-    def accumulate_glow(carry, values):
-        glow_grid = jnp.where(values > 0.01, values + carry, carry)
-        return glow_grid, None
-
-    # Scan across fireflies to create a composite image with additive glow
-    final_glow_grid = jax.lax.scan(accumulate_glow, glow_grid, glow_values)[0]
+    coordinates = jnp.stack([xs, ys], axis=-1)  # Get locations for each fireflies
+    blink_durations = state_durations * blinks  # Determine if / how long they've been blinking for
+    glow_sizes = jnp.where(blink_durations < MAX_GLOW_SIZE, blink_durations, MAX_GLOW_SIZE) # Cap their glow sizes
+    glow_values = jax.vmap(gaussian_2d, in_axes=(0, 0))(coordinates, glow_sizes) # Draw each firefly
+    final_glow_grid = jnp.sum(jnp.where(glow_values > 0.1, glow_values, 0), axis=0) # Combine the frames
     
     return final_glow_grid
